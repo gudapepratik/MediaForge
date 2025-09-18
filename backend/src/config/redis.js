2296793696f184c02a602e131271a429cbbb7a8f
Pratik <pratikgudape9825@gmail.com>
2025-09-18 19:21:37 +0530

    - redis client setup done - new docker service for redis setup done

---
+++ backend/src/config/redis.js
@@ -0,0 +1,17 @@
+import IORedis from 'ioredis'
+
+// DOC - https://redis.github.io/ioredis/
+
+const connection = new IORedis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || '6379',
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false
+})
+
+connection.on('connect', () => console.log("REDIS CONNECTED"))
+connection.on('error', (err) => console.error("REDIS CONNECTION ERROR", err))
+
+export default connection;
+
