cbc479b01fd106d0d3f63df754e6e01ae53d4a5c
Pratik <pratikgudape9825@gmail.com>
2025-10-05 00:51:39 +0530

    - video player added - video.js is used

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,50 +1,74 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
-function VideoCard({ video = null }) {
-  const videodummy = {
-    thumbnail:
-      "https://i.ytimg.com/vi/ysz5S6PUM-U/maxresdefault.jpg",
-    title: "This is the video title for testing purposes",
-    channel: "Dummy Channel",
-    duration: "12:34",
-    views: "1.2M",
-    uploaded: "2 days ago",
-  };
+function VideoCard({ video = null, options, onReady }) {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
 
-  const data = video || videodummy;
+  useEffect(() => {
+    if(!playerRef.current) {
+      const videoElement = document.createElement('video-js')
+      videoElement.classList.add('vjs-big-play-centered')
+      videoRef.current.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+        if (onReady) {
+          onReady(player);
+        }
+      }));
+    } else{
+      const player = playerRef.current;
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+    
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [options, onReady]);
 
   return (
-    <div className="max-w-sm rounded-2xl overflow-hidden bg-zinc-100 hover:shadow-lg transition p-2">
-      {/* Thumbnail */}
-      <div className="relative">
-        <img
-          src={data?.thumbnail || "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg"}
-          alt={data?.title}
-          className="w-full rounded-lg"
-        />
-        <span className="absolute bottom-2 right-2 bg-black bg-opacity-10 text-white text-xs px-1.5 py-0.5 rounded">
-          {data?.duration}
-        </span>
-      </div>
+    // <div className="max-w-sm rounded-2xl overflow-hidden bg-zinc-100 hover:shadow-lg transition p-2">
+    //   {/* Thumbnail */}
+    //   <div className="relative">
+    //     <img
+    //       src={data?.thumbnail || "https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg"}
+    //       alt={data?.title}
+    //       className="w-full rounded-lg"
+    //     />
+    //     <span className="absolute bottom-2 right-2 bg-black bg-opacity-10 text-white text-xs px-1.5 py-0.5 rounded">
+    //       {data?.duration}
+    //     </span>
+    //   </div>
 
-      {/* Video Info */}
-      <div className="flex mt-3">
-        {/* Channel Avatar */}
-        <img
-          src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg"
-          alt={data?.channel}
-          className="w-10 h-10 rounded-full"
-        />
+    //   {/* Video Info */}
+    //   <div className="flex mt-3">
+    //     {/* Channel Avatar */}
+    //     <img
+    //       src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg"
+    //       alt={data?.channel}
+    //       className="w-10 h-10 rounded-full"
+    //     />
 
-        {/* Title & Channel */}
-        <div className="ml-3">
-          <h3 className="text-sm font-semibold line-clamp-2">{data?.title}</h3>
-          <p className="text-xs text-gray-600">{data?.channel}</p>
-          <p className="text-xs text-gray-500">
-            {data?.views} views • {data?.uploaded}
-          </p>
-        </div>
-      </div>
+    //     {/* Title & Channel */}
+    //     <div className="ml-3">
+    //       <h3 className="text-sm font-semibold line-clamp-2">{data?.title}</h3>
+    //       <p className="text-xs text-gray-600">{data?.channel}</p>
+    //       <p className="text-xs text-gray-500">
+    //         {data?.views} views • {data?.uploaded}
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className='video-player' data-vjs-player>
+      <div ref={videoRef} />
     </div>
   );
 }
--- frontend/src/pages/Videos.jsx
+++ frontend/src/pages/Videos.jsx
@@ -9,6 +9,20 @@ function Videos() {
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const hlsOptions = {
+    autoplay: false,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    preload: "auto",
+    sources: [
+      {
+        src: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
   const fetchVideos = async () => {
     try {
       setIsLoading(true);
@@ -16,7 +30,10 @@ function Videos() {
         withCredentials: true
       })
       console.log(data.data.videos);
-      setVideos(data.data.videos);
+      const videosData = data.data.videos?.map((video) => ({...video, hlsOptions: {...hlsOptions, sources: [{src: `https://pub-b462f8f0e6784b8fbdbfca6e0cd1d5cb.r2.dev/${video.storageKey}`, type: 'application/x-mpegURL'}]}}))
+      console.log(videosData);
+      setVideos(videosData);
+
     } catch (error) {
       console.log('Error fetching videos', error);
     } finally{
@@ -40,7 +57,7 @@ function Videos() {
           <div className='w-full grid grid-cols-3 p-4 gap-4 h-screen '>
             {videos && (
               videos.map((video, key) => (
-                <VideoCard key={video.id} video={video}/>
+                <VideoCard key={key} video={video} options={video.hlsOptions}/>
               ))
             )}
           </div>
